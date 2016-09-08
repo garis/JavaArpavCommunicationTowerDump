747679db1f3ebf21884b2e1fa2a96e5cb2c24bed
rgasparini <garis94@gmail.com>
2016-09-08 09:52:16 +0200

    Migliorato il parsing della pagine per accomodare delle stranezze tipo: http://map.arpa.veneto.it/contents/agenti_fisici/htm/scheda.jsp?id_sito=8814 più caso particolare nel caso single thread con id di inizio e fine specificati a mano

--- src/arpavnetworktowerdump/ArpavNetworkTowerDump.java
+++ src/arpavnetworktowerdump/ArpavNetworkTowerDump.java
@@ -12,17 +12,30 @@ public class ArpavNetworkTowerDump {
     public static int TARGET_ID = 35000; //ID massimo a cui arrivare
 
     public static int THREAD_NUMBER = 1;
-    public static int THREAD_SLEEP_MS = 150; //quanto riposa il thread prima di ricominciare
+    public static int THREAD_SLEEP_MS = 500; //quanto riposa il thread prima di ricominciare
     public static int THREAD_SLEEP_ERROR = 500;//tempo di attesa dopo una ecezzione
     public static int FOUNDED = 0;
     public static int RANDOMIZE_TIME = 0;
     public static int RETRY = 10;
 
+    public static int MIN_ID = 0;
+    public static int MAX_ID = 0;
+
     public static int threadCompleted = 0;
 
     public static void main(String[] args) throws IOException {
-        ArpavNetworkTowerDump thisClass = new ArpavNetworkTowerDump();
-        thisClass.setAndStart();
+        if (args.length == 3) {
+            THREAD_NUMBER = 1;
+            System.out.println("FROM: " + args[1]);
+            System.out.println("TO: " + args[2]);
+            MIN_ID = Integer.parseInt(args[1]);
+            MAX_ID = Integer.parseInt(args[2]);
+            ArpavNetworkTowerDump thisClass = new ArpavNetworkTowerDump();
+            thisClass.setAndStart();
+        } else {
+            ArpavNetworkTowerDump thisClass = new ArpavNetworkTowerDump();
+            thisClass.setAndStart();
+        }
     }
 
     public class Writer {
@@ -33,7 +46,7 @@ public class ArpavNetworkTowerDump {
 
         public Writer() {
             try {
-                writer = Files.newBufferedWriter(Paths.get("towers.csv"), charset);
+                writer = Files.newBufferedWriter(Paths.get("towersFROM" + MIN_ID + "TO" + MAX_ID + ".csv"), charset);
             } catch (IOException x) {
                 System.err.format("IOException: %s%n", x);
             }
@@ -69,13 +82,20 @@ public class ArpavNetworkTowerDump {
 
     public void setAndStart() {
         Writer writerClass = new Writer();
-
         DumperThread[] vectorThread = new DumperThread[THREAD_NUMBER];
-        int workForThread = TARGET_ID / THREAD_NUMBER;
-        for (int i = 0; i < THREAD_NUMBER; i++) {
-            vectorThread[i] = new DumperThread();
-            vectorThread[i].setWriter(writerClass, workForThread * i, workForThread * i + workForThread, i);
-            vectorThread[i].start();
+
+        if (THREAD_NUMBER == 1) {
+            vectorThread[0] = new DumperThread();
+            vectorThread[0].setWriter(writerClass, MIN_ID, MAX_ID, 0);
+            vectorThread[0].start();
+
+        } else {
+            int workForThread = TARGET_ID / THREAD_NUMBER;
+            for (int i = 0; i < THREAD_NUMBER; i++) {
+                vectorThread[i] = new DumperThread();
+                vectorThread[i].setWriter(writerClass, workForThread * i, workForThread * i + workForThread, i);
+                vectorThread[i].start();
+            }
         }
     }
 
@@ -142,21 +162,20 @@ public class ArpavNetworkTowerDump {
 
     public void getPage(Writer writer, String pageAddress, int number) throws MalformedURLException, IOException {
         URL towerPage = new URL(pageAddress);
-        BufferedReader inBuffer = null;
+        BufferedReader inBuffer;
         inBuffer = new BufferedReader(
                 new InputStreamReader(towerPage.openStream()));
 
-        String[] pageLine = new String[8];
+        String page = "";
         String temp;
-        int i = -1;
-        while (i < 8 && (temp = inBuffer.readLine()) != null) {
+        int i = 0;
+        while (i != 2 && (temp = inBuffer.readLine()) != null) {
             //scarta tutto cio che c'e prima di "Codice Sito:"
             if (temp.contains("Codice Sito:")) {
                 i++;
             }
-            if (i >= 0 && i < pageLine.length) {
-                pageLine[i] = temp;
-                i++;
+            if (i == 1) {
+                page = page + temp;
             }
             //scarta tutto cio che c'e dopo di "Altezza centro elettrico dal suolo"
             if (temp.contains("Altezza centro elettrico dal suolo")) {
@@ -164,73 +183,74 @@ public class ArpavNetworkTowerDump {
             }
         }
         inBuffer.close();
-        if (i > 0) {
-            dumpInfo(writer, pageLine, number);
+        if (i == 2) {
+            dumpInfo(writer, page, number);
         } else {
             System.out.println(number + "       not found");
         }
     }
 
-    public void dumpInfo(Writer writer, String[] pageData, int number) {
+    public void dumpInfo(Writer writer, String pageData, int number) {
         String[] temp1;
+        String[] temp2;
 
         String line = ("" + number).concat("%");
 
-        if (pageData.length == 8) {
-            for (int i = 0; i < pageData.length; i++) {
-                //Codice Sito:
-                if (i == 0) {
-                    temp1 = pageData[i].split("</b>");
-                    temp1 = temp1[1].split("</h2>");
-                    line = line + temp1[0].concat("%");
-                }
-                //Nome
-                if (i == 1) {
-                    temp1 = pageData[i].split("</b>: ");
-                    line = line + temp1[1].concat("%");
-                }
-                //Gestore, Indirizzo, Quota al suolo, Postazione
-                if (i == 2 || i == 3 || i == 6) {
-                    temp1 = pageData[i].split("</b>: ");
-                    if (i == 3) {
-                        temp1[1] = temp1[1].replaceAll("&quot;", "");
-                    }
-                    line = line + temp1[1].concat("%");
-                }
-                if (i == 5) {
-                    temp1 = pageData[i].split(": ");
-                    temp1 = temp1[1].split("</p>");
-                    temp1 = temp1[0].split(" m s.l.m.");
-                    line = line + temp1[0].concat("%");
-                }
-                //Coordinate
-                if (i == 4) {
-                    double x = 0, y = 0;
-                    temp1 = pageData[i].split(": ");
-                    temp1 = temp1[1].split(" x; ");
-                    x = Double.parseDouble(temp1[0]);
-                    temp1 = temp1[1].split(" y");
-                    y = Double.parseDouble(temp1[0]);
+        try {
+            temp1 = pageData.split("<b>");
 
-                    double[] lat_lon = UTMtoLatLon.toLatLon(x, y, "N");
-                    line = line + lat_lon[0] + "%" + lat_lon[1] + "%";
+            //Codice Sito
+            temp2 = temp1[1].split("</b>");
+            temp2 = temp2[1].split("</h2>");
+            line = line + temp2[0].concat("%");
 
-                }
-                //Altezza
-                if (i == 7) {
-                    temp1 = pageData[i].split(": ");
-                    temp1 = temp1[1].split("</p>");
-                    line = line + temp1[0].concat("%");
-                }
-            }
+            //Nome
+            temp2 = temp1[2].split("</b>: ");
+            temp2 = temp2[1].split("</p>");
+            line = line + temp2[0].concat("%");
+
+            //Gestore
+            temp2 = temp1[3].split("</b>: ");
+            temp2 = temp2[1].split("</p>");
+            line = line + temp2[0].concat("%");
+
+            //Indirizzo
+            temp2 = temp1[4].split("</b>: ");
+            temp2 = temp2[1].split("</p>");
+            line = line + temp2[0].concat("%");
+
+            //Coordinate
+            double x = 0, y = 0;
+            temp2 = temp1[5].split(": ");
+            temp2 = temp2[1].split(" x; ");
+            x = Double.parseDouble(temp2[0]);
+            temp2 = temp2[1].split(" y");
+            y = Double.parseDouble(temp2[0]);
+            double[] lat_lon = UTMtoLatLon.toLatLon(x, y, "N");
+            line = line + lat_lon[0] + "%" + lat_lon[1] + "%";
+
+            //Quota al suolo
+            temp2 = temp1[6].split("</b>: ");
+            temp2 = temp2[1].split("m s.l.m.");
+            line = line + temp2[0].concat("%");
+
+            //Postazione
+            temp2 = temp1[7].split("</b>: ");
+            temp2 = temp2[1].split("</p>");
+            line = line + temp2[0].concat("%");
+
+            //Altezza centro elettrico dal suolo
+            temp2 = temp1[8].split(": ");
+            temp2 = temp2[1].split("</p>");
+            line = line + temp2[0].concat("%");
+
             try {
                 writer.write(line);
             } catch (IOException ex) {
                 Logger.getLogger(ArpavNetworkTowerDump.class.getName()).log(Level.SEVERE, null, ex);
             }
-            System.out.println(number + "       dumped");
-        } else {
-            System.out.println(number + "       problem");
+        } catch (Exception ex) {
+            System.out.println(number + "       Errore");
         }
     }
 }
